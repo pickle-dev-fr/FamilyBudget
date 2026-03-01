b900e7ea645405ee82b2b16cc46716c196489c46
pickle-dev-fr <pickle-dev-fr@github.com>
2026-03-01 19:11:10 +0100

    style: correction for mobile view

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,4 +1,5 @@
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+console.log(API_URL)
 
 export type ApiError = {
     status: number;
--- frontend/src/components/layout/AppLayout.tsx
+++ frontend/src/components/layout/AppLayout.tsx
@@ -3,20 +3,46 @@ import PrivateHeader from "./PrivateHeader";
 import Menu from "./Menu";
 
 export default function AppLayout() {
+    return (
+        <div className="drawer lg:drawer-open h-screen">
+            <input
+                id="app-drawer"
+                type="checkbox"
+                className="drawer-toggle"
+            />
 
-  return (
-    <div className="flex flex-col h-screen bg-bg text-text">
-        <PrivateHeader />
+            {/* CONTENT */}
+            <div className="drawer-content flex flex-col h-full bg-bg text-text">
+                <PrivateHeader />
 
-        <div className="flex flex-1 overflow-hidden">
-            <aside className="w-60 bg-bg-soft p-4 overflow-y-auto">
-                <Menu />
-            </aside>
+                {/* Mobile navbar */}
+                <div className="navbar bg-base-100 lg:hidden">
+                    <div className="flex-none">
+                        <label
+                            htmlFor="app-drawer"
+                            className="btn btn-square btn-ghost"
+                        >
+                            ☰
+                        </label>
+                    </div>
+                </div>
 
-            <main className="flex-1 p-4 overflow-y-auto">
-                <Outlet />
-            </main>
+                <main className="flex-1 overflow-y-auto p-4 lg:p-6">
+                    <Outlet />
+                </main>
+            </div>
+
+            {/* SIDEBAR */}
+            <div className="drawer-side">
+                <label
+                    htmlFor="app-drawer"
+                    className="drawer-overlay"
+                ></label>
+
+                <aside className="w-72 min-h-full h-full bg-base-200 p-4 overflow-y-auto">
+                    <Menu />
+                </aside>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
